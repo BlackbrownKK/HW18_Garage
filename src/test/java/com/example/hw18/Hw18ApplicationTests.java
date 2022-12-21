3dcefb1cf3d86efeec3f6afd8a9a26748bf3195e
k.kosteniuk <konstantinkostenyuk@gmail.com>
2022-12-21 09:17:29 +0200

    inition commit

---
+++ src/test/java/com/example/hw18/Hw18ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.hw18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw18ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
